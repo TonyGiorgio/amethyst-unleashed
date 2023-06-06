e72c6b8bfb7d6857c623ed640d44476bdff8ef8d
Tony Giorgio <tonygiorgio@protonmail.com>
2023-06-05 22:44:19 -0500

    Onyx rebrand

--- app/src/main/java/com/vitorpamplona/amethyst/ui/theme/Theme.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/theme/Theme.kt
@@ -268,11 +268,7 @@ val Colors.replyModifier: Modifier
 
 @Composable
 fun AmethystTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
+    val colors = DarkColorPalette
 
     MaterialTheme(
         colors = colors,
